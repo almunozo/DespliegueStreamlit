67fc5fac587b012a9177dd727eb36fdf9956c77a
Alejandro Esteban Muñoz Osorio <almunozo@unal.edu.co>
2023-10-10 18:30:00 -0500

    g

--- 1_🤓_Homepage.py
+++ 1_🤓_Homepage.py
@@ -18,6 +18,7 @@ if "my_input" not in st.session_state:
 my_input = st.text_input("Input a text here", st.session_state["my_input"])
 
 submit = st.button("Submit")
+
 if submit:
     st.session_state["my_input"] = my_input
     st.write("You have entered: ", my_input)
